243ee338cb040175dbd0cbf76a212f1e17858ef2
DEMG <demg@outlook.com>
2017-01-14 18:43:03 -0600

    -Suponiendo como funciona Kotlin, he creado un metodo llamado "variables()" este metodo contiene lo practicado con variables y mensajes.

--- src/app.kt
+++ src/app.kt
@@ -3,5 +3,26 @@
  */
 
 fun main(args: Array<String>) {
+    variables()
+}
+
+fun variables() {
     println("Hola Mundo")
+
+    // De forma sencilla
+    var a = 1
+    // De forma explisita
+    var aa: Int = 1
+
+    // Declarar variable y luego asignarla
+    var aaa: Int
+    aaa = 3
+    var b = 5
+    b += 1
+
+    // Constantes
+    val bb = 5
+
+    println(aaa + b)
+
 }
